a29d3c14008f2cc6fac44204997af2e3f98c50cb
Michael Salvati <print@3daddict.com>
2018-12-07 14:50:00 -0800

    almost completed stuck on reset the stored numbers

--- exercises/basiccalcobject.js
+++ exercises/basiccalcobject.js
@@ -3,28 +3,69 @@ class Calculator{
 	constructor(){
 		//takes in nothing
 		//make storage for the operator and the numbers
+		this.operator = '';
+		this.numbers = [];
+		this.result = null;
 	}
-	loadOperator(  ){
+	loadOperator( operator ){
 		//adds the operator to the next calculation
 		//takes in the operator
 		//checks if it is a valid operation (+-*/)
+		if (operator === '+' || operator === '-' || operator === '*' || operator === '/') {
 			//save the op to the constructor storage
+			this.operator = operator;
 			//return true
-		//or return false if not the right operator
+			return true;
+		} else {
+			//or return false if not the right operator
+			return false;
+		}
 	}
-	loadNumber(  ){
+	loadNumber( numbers ){
 		//takes in a number and stores it as one of the numbers to perform math on
 		//takes in 1 number
 		//checks if it is actually a number and if we have fewer than 2 numbers
-		//if it is a number, and we have 2 or fewer numbers, store it
+		if (Number.isInteger(numbers) && this.numbers.length <= 2) {
+			numbers += this.numbers
+			//if it is a number, and we have 2 or fewer numbers, store it
+			let numArr = (""+numbers).split(""); //converting to array to get length and store
+			this.numbers = numArr;
 			//return the number of numbers stored
-		//otherwise return false (too many numbers stored)
+			console.log('numArr length: ', numArr.length);
+			console.log("numArr: ", numArr);
+			return numArr.length;
+		} else {
+			//otherwise return false (too many numbers stored)
+			return false;
+		}
 	}
 	calculate(){
 		//calculate the result of the stored numbers and operator
 		//takes in nothing
 		//calculates with the operator and 2 numbers
+		console.log('Calculate Numbers:', this.numbers);
+		console.log('Calculate Operator:', this.operator);
+		switch (this.operator) {
+			case "+":
+				this.result = this.numbers[0] + this.numbers[1];
+				break;
+			case "-":
+				this.result = this.numbers[0] - this.numbers[1];
+				break;
+			case "*":
+				this.result = this.numbers[0] * this.numbers[1];
+				break;
+			case "/":
+				this.result = this.numbers[0] / this.numbers[1];
+				break;
+			default:
+				return false;
+		}
+		console.log('Result:', this.result);
 		//clears out the stored numbers
+		this.numbers[0] = null;
+		this.numbers[1] = null;
 		//returns the calculated result
+		return this.result;
 	}
 }
