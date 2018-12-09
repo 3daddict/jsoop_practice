09a2694af06328f740a3cbf19e6cffa3460343d9
Michael Salvati <print@3daddict.com>
2018-12-08 20:44:16 -0800

    working on bank.js

--- exercises/bank.js
+++ exercises/bank.js
@@ -1,37 +1,61 @@
 
 
 class Bank{
-	constructor(){
+	constructor(bankName){
 		//takes in the name of the bank.
 		//makes an object to store all accounts into
+		this.bankName = bankName;
+		this.bankAccounts = {};
 	}
-	makeAccount( ){
+	makeAccount( accountNumber ){
 		//makes an account
 		//takes in the account number of an account
 		//checks if the account number already exists in the account storage object in the constructor
+		if(this.bankAccounts.hasOwnProperty(accountNumber)) {
 			//if already exists, returns false
-		//otherwise makes an new Account object, storing it into the storage object in the constructor
-		//returns the Account object that was constructed
+			return false;
+		} else {
+			//otherwise makes an new Account object, storing it into the storage object in the constructor
+			const newAccount = new Account();
+			//returns the Account object that was constructed
+			return this.bankAccounts[accountNumber] = newAccount;
+		}
+			
 	}
-	checkForAccount(  ){
+	checkForAccount( accountNumber ){
 		//checks if an account exists or not
 		//takes in the account number of an account
+		if(this.bankAccounts.hasOwnProperty(accountNumber)) {
 		//returns false if the account does not exist in the accounts object from the constructor
 		//returns true if the account does exist
+			return true;
+		} else {
+			return false;
+		}
+		
 	}
-	removeAccount(  ){
+	removeAccount( accountNumber ){
 		//removes an account
 		//takes in an account number
-		//if the account doesn't exist, returns
-			//"account <accountNumber> does not exist" where accountNumber is the account number
-		//if the account is not empty, returns
+		if(this.bankAccounts.hasOwnProperty(accountNumber)) {
+			//if the account is not empty, returns
 			//'account is not empty'
-		//otherwise deletes the account from the constructor storage
-		//returns 
-			'account <accountNumber> deleted'
+			if(this.bankAccounts.accountNumber > 0){
+				return 'account is not empty';
+			} else {
+				//otherwise deletes the account from the constructor storage
+				delete this.bankAccounts[accountNumber];
+				//returns 
+				return "account " + accountNumber + " deleted"
+			}
+		} else {
+			//if the account doesn't exist, returns
+			//"account <accountNumber> does not exist" where accountNumber is the account number
+			return "account " + accountNumber +  " does not exist"
+		}
 
 	}
-	deposit( ){
+	deposit( accountNumber, depositAmount ){
 		//deposits money into an account
 		//takes in an account number and a numeric amount
 		//if the account doesn't exist, returns
@@ -40,7 +64,7 @@ class Bank{
 			//returns
 			//"account <accountNumber> now has <new account amount>
 	}
-	withdraw( accountNumber, amount ){
+	withdraw( accountNumber, withdrawAmount ){
 		//removes money from an account
 		//takes in an account number and an amount
 		//checks if the account exists, if not
@@ -49,3 +73,7 @@ class Bank{
 		//returns "removed <amount withdrawn> from account <account number>. It now has <remaining amount in account>"
 	}
 }
+
+const testBank = new Bank;
+testBank.makeAccount('32457639845');
+testBank.removeAccount('32457639845');
